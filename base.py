dc194ee1701b1a797a2cc76794c44be2427be000
Luke Samuel <lukesam9@gmail.com>
2022-09-06 08:48:19 +0530

    Revisiting PUSH topic

--- base.py
+++ base.py
@@ -1,3 +1,11 @@
 # this is a base python file used to teach pull.
 
 "Hello everyone this is Luke 2 "
+
+"""
+1. Remote Repo 
+2. Local Repo 
+"""
+
+'case 1 : Remote Repo has latest update and local repo is old code -> PULL'
+'case 2 : Local Repo has latest update and Remote repo is old code -> PUSH'
