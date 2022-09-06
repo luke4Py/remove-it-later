66b4649408aaf966e5bba31be3f03b15c0687669
codemonk <60259430+luke4Py@users.noreply.github.com>
2022-09-06 08:53:53 +0530

    Updating code with sample program with choice

--- unbase.py
+++ unbase.py
@@ -1,1 +1,7 @@
 'Hello Everyone, this is luke and the project file name is unbase.py from local repo 2'
+
+from math.random import choice
+
+list_students = ['Vamshi', 'Satabdi', 'Naveena', 'Christina', 'Kishore']
+
+choice(list_students)
