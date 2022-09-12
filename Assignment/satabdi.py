c48ab393505efa3848feb95e7595e99325490407
satabdi <deysatabdi70@gmail.com>
2022-09-12 12:42:20 +0530

    satabdi's assignment on if-else statement

--- Assignment/satabdi.py
+++ Assignment/satabdi.py
@@ -9,9 +9,9 @@ if gender == "male":
         print("Section3")
 elif gender == "female":
     if age>=1 and age<=10:
-        print("Section1")
+        print("Section4")
     elif age>=11 and age<=20:
-        print("Section2")
+        print("Section5")
     else:
-        print("Section3")
+        print("Section6")
 
