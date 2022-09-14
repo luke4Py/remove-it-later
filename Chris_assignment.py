86f44d8d5dd03e7548d2224b094d1cfa48bf6ab9
christinaserveas <christinaserveas@gmail.com>
2022-09-14 10:01:23 +0530

    assignment_operators

--- Chris_assignment.py
+++ Chris_assignment.py
@@ -33,17 +33,17 @@ Input_age = int(input())
 Input_gender = input("Enter your gender")
 
 if Input_gender == "male":
-   if Input_age in range(1-10):
+   if Input_age in range(1, 10):
       print("Belongs to group 1")
-   elif Input_age in range(11-20):
+   elif Input_age in range(11, 20):
         print("Belongs to group 2")
         
    else:
       print("Belongs to group 3")
 if Input_gender == "female":
-   if Input_age in range(1-10):
+   if Input_age in range(1, 10):
         print("Belongs to group 4")
-   elif Input_age in range(11 - 20):
+   elif Input_age in range(11, 20):
         print("Belongs to group 5")
    else: 
     print("Belongs to group 6")
